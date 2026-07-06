8ed4c7b6a50a352e8b116aaf93a901178502f255
Izaak Sarnecki <izaaksarnecki@outlook.com>
2026-07-06 21:43:24 +0200

    init chip8 struct

---
+++ chip8.h
@@ -0,0 +1,30 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#define NUM_REGISTERS 16
+#define MEM_SIZE 4096
+#define STACK_SIZE 16
+#define NUM_INPUT_KEYS 16
+#define VIDEO_WIDTH 64
+#define VIDEO_HEIGHT 32
+
+#include <stdint.h>
+
+typedef struct {
+  uint8_t registers[NUM_REGISTERS];
+  uint8_t memory[MEM_SIZE];
+  uint16_t index_reg;
+  uint16_t pc;
+  uint16_t stack[STACK_SIZE];
+  uint8_t sp;
+  uint8_t delay_timer;
+  uint8_t sound_timer;
+  uint8_t keypad[NUM_INPUT_KEYS];
+  uint32_t videobuffer[VIDEO_WIDTH * VIDEO_HEIGHT]; // use 32bit for sdl
+                                                    // compatability later on
+  uint16_t opcode;
+} chip8;
+
+void chip8_init(chip8 *c);
+
+#endif // CHIP8_H
